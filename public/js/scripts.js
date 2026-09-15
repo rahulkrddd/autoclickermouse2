835d33de138f763a775271e37a199a68d57bd6ad
rahulkrddd <rahul.gupta.730@gmail.com>
2026-09-15 11:01:06 +0530

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 // Global variables for product price
 let productPrice = 999; // Define the product price
-let collectGatewayCharges = "Y"; // Define whether to collect gateway charges ('Y' or 'N')
+let collectGatewayCharges = "N"; // Define whether to collect gateway charges ('Y' or 'N')
 let gatewayChargePercentage = 1.0218; // Razorpay charges ~2.18%
 
   document.addEventListener("DOMContentLoaded", () => {
